6dfc58d7bd7e86e8e3243e1ee233deb20945535d
martin-k-kamir <martin.k.kamir@gmail.com>
2022-08-09 00:44:51 +0200

    w

--- src/App.js
+++ src/App.js
@@ -1,81 +1,78 @@
 export default function App() {
 	return (
 		<div className="App">
-			<nav className="container">
-				<div className="container">
-					<ul className="navbar">
-						<li className="navbar__text"><a href="/">Models</a></li>
-						<li className="navbar__text"><a href="/">Custom solutions</a></li>
-						<li className="navbar__text"><a href="/">ownership</a></li>
-						<li className="navbar__text"><a href="/">motorsport</a></li>
-					</ul>
-				</div>
-			</nav>
-			<section></section>
-			<section></section>
-			<section></section>
-			<section></section>
-			<footer></footer>
-			{/*/!* Navbar*!/*/}
+			<section className="section-navbar">
+				<ul className="navbar">
+					<li className="navbar__text"><a href="/">Models</a></li>
+					<li className="navbar__text"><a href="/">Custom solutions</a></li>
+					<li className="navbar__text"><a href="/">ownership</a></li>
+					<li className="navbar__text"><a href="/">motorsport</a></li>
+				</ul>
+				<ul className="navbar-drop">
+					<li className="navbar__subtext"><a href="/">Aventador</a></li>
+					<li className="navbar__subtext"><a href="/">Huracán</a></li>
+					<li className="navbar__subtext"><a href="/">Urus</a></li>
+					<li className="navbar__subtext"><a href="/">Pre-Owned</a></li>
+				</ul>
+				<p className="navbar__text-extended"><a href="/">Design</a></p>
+				<p className="navbar__text-extended-title">LANGUAGES</p>
+				<p className="navbar__text-extended-text">ENGLISH</p>
+			</section>
+			<section className="section-search">
+				<p className="search__subtext">LAMBORGHINI</p>
+				<p className="search__text">SEARCH IN THE WEBSITE</p>
+			</section>
+			<section className="section-cover">
+				<h1 className="title-1">
+					<span className="subtitle">huracán tecnica</span>
+					<span className="title">Take all your souls to drive</span>
+				</h1>
+			</section>
+			<section className="section-models">
+				<p className="title-5">Models</p>
+				<h2 className="title-1">
+					<span className="title">huracán</span>
+					<span className="subtitle">take all your souls to drive</span>
+				</h2>
+			</section>
+			<section className="section-models">
+				<p className="title-5">Configurator</p>
+				<h2 className="title-2">Create your urus</h2>
+			</section>
+			<section className="section-blog">
+				<h2 className="title-1 measure-7">
+					<span className="subtitle">news</span>
+					<span className="title">LAMBORGHINI WORLD</span>
+				</h2>
 
-			{/*/!*navbar sub*!/*/}
-			{/*<ul className="navbar">*/}
-			{/*	<li className="navbar__subtext"><a href="/">Aventador</a></li>*/}
-			{/*</ul>*/}
-			{/*/!*search subtitle*!/*/}
-			{/*<p className="search__subtext">LAMBORGHINI</p>*/}
-			{/*/!*search title*!/*/}
-			{/*<p className="search__text">SEARCH IN THE WEBSITE</p>*/}
-			{/*/!*navbar extended *!/*/}
-			{/*<ul className="navbar">*/}
-			{/*	<li className="navbar__text-extended"><a href="/">Design</a></li>*/}
-			{/*</ul>*/}
-			{/*/!*navbar extended down title *!/*/}
-			{/*<p className="navbar__text-extended-title">LANGUAGES</p>*/}
-			{/*/!*navbar extended down text *!/*/}
-			{/*<p className="navbar__text-extended-text">ENGLISH</p>*/}
+			</section>
+			<section className="section-blog">
+				<p className="blog_date">5 august 2022</p>
+				<p className="blog_title">A NEW CHAPTER FOR LAMBORGHINI’S NFT JOURNEY</p>
+				<p className="blog_desc">Automobili Lamborghini’s collaboration with NFT PRO™ and INVNT continues with a new chapter.</p>
+			</section>
+			<section className="section-blog">
+				<p className="blog_date--small">5 august 2022</p>
+				<p className="blog_tag">HURACÁN</p>
+				<p className="blog_title">LAMBORGHINI MAGAZINE #30: THE PLAYLIST</p>
+			</section>
+			<section className="section-footer">
+				<ul className="footer">
+					<li className="footer__text"><a href="/">Company</a></li>
+					<li className="footer__text"><a href="/">Careers</a></li>
+					<li className="footer__text"><a href="/">Contact us</a></li>
+					<li className="footer__text"><a href="/">Media center</a></li>
+				</ul>
 
-			{/*/!*Cover*!/*/}
-			{/*<p className="cover-subtext">Huracan tecnica</p>*/}
-			{/*<p className="cover-text">Take all your souls to drive</p>*/}
+				<p className="footer__desc-sm">The consumption and emissions values in the website refer to your
+					geographical IP. This
+					value might be unrealistic if you navigate using VPN or if the position of your Internet provider is
+					imprecise. If you believe you are incorrectly geolocalized, contact your dealer to get valid consumption
+					and emissions information in your area.</p>
 
-			{/*/!*models*!/*/}
-			{/*<p className="models__top-text">Models</p>*/}
-			{/*<p className="models__top-text-title">Huracán</p>*/}
-			{/*<p className="models__top-text-subtitle">TAKE ALL YOUR SOULS TO DRIVE</p>*/}
-
-			{/*/!*Config*!/*/}
-			{/*<p className="config__text-top">Configurator</p>*/}
-			{/*<p className="config__text-title">Create your urus</p>*/}
-
-			{/*/!*news*!/*/}
-			{/*<p className="models__top-text">NEWS</p>*/}
-			{/*<p className="models__top-text-title">LAMBORGHINI WORLD</p>*/}
-
-			{/*/!*blog*!/*/}
-			{/*<p className="blog_date">5 august 2022</p>*/}
-			{/*<p className="blog_title">A NEW CHAPTER FOR LAMBORGHINI’S NFT JOURNEY</p>*/}
-			{/*<p className="blog_desc">Automobili Lamborghini’s collaboration with NFT PRO™ and INVNT continues with a new*/}
-			{/*	chapter.</p>*/}
-
-			{/*<p className="blog_date--small">5 august 2022</p>*/}
-			{/*<p className="blog_tag">HURACÁN</p>*/}
-			{/*<p className="blog_title">LAMBORGHINI MAGAZINE #30: THE PLAYLIST</p>*/}
-
-			{/*/!*Footer*!/*/}
-			{/*<ul className="footer">*/}
-			{/*	<li className="footer__text"><a href="/">Company</a></li>*/}
-			{/*	<li className="footer__text"><a href="/">Careers</a></li>*/}
-			{/*	<li className="footer__text"><a href="/">Contact us</a></li>*/}
-			{/*	<li className="footer__text"><a href="/">Media center</a></li>*/}
-			{/*</ul>*/}
-			{/*<p className="footer__desc-sm">The consumption and emissions values in the website refer to your*/}
-			{/*	geographical IP. This*/}
-			{/*	value might be unrealistic if you navigate using VPN or if the position of your Internet provider is*/}
-			{/*	imprecise. If you believe you are incorrectly geolocalized, contact your dealer to get valid consumption*/}
-			{/*	and emissions information in your area.</p>*/}
-			{/*<p className="footer__desc">Copyright © 2022 Automobili Lamborghini S.p.A. a sole shareholder company part of Audi Group. All rights*/}
-			{/*	reserved. VAT no. IT 00591801204</p>*/}
+				<p className="footer__desc">Copyright © 2022 Automobili Lamborghini S.p.A. a sole shareholder company part of Audi Group. All rights
+					reserved. VAT no. IT 00591801204</p>
+			</section>
 		</div>
 	);
 }
