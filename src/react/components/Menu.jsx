4ded30209e27fdbd151fcac2af013592f90f19ab
Martin Kamír <martin.k.kamir@gmail.com>
2022-08-17 17:45:43 +0200

    w

--- src/react/components/Menu.jsx
+++ src/react/components/Menu.jsx
@@ -6,23 +6,23 @@ export default function Menu(props) {
 			<div className="app-container">
 				<div className="menu__container">
 					<ul className="menu__grid" role="list">
-						{/*<li className="menu__item"><a className="link-2" href="/">models</a></li>*/}
-						{/*<li className="menu__item"><a className="link-2" href="/">custom solutions</a></li>*/}
-						{/*<li className="menu__item"><a className="link-2" href="/">ownership</a></li>*/}
-						{/*<li className="menu__item"><a className="link-2" href="/">motorsport</a></li>*/}
-						{/*<li className="menu__item"><a className="link-2" href="/">dealership</a></li>*/}
-						{/*<li className="menu__item"><a className="link-2" href="/">museum</a></li>*/}
-						{/*<li className="menu__item"><a className="link-2" href="/">store</a></li>*/}
-						<li className="menu__item"><a className="link-2 link-underline" href="/">design</a></li>
-						<li className="menu__item"><a className="link-2 link-underline" href="/">innovation & excellence</a></li>
-						<li className="menu__item"><a className="link-2 link-underline" href="/">sustainability</a></li>
-						<li className="menu__item"><a className="link-2 link-underline" href="/">driving programs</a></li>
-						<li className="menu__item"><a className="link-2 link-underline" href="/">lounge</a></li>
-						<li className="menu__item"><a className="link-2 link-underline" href="/">club</a></li>
-						<li className="menu__item"><a className="link-2 link-underline" href="/">history</a></li>
-						<li className="menu__item"><a className="link-2 link-underline" href="/">news</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">models</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">custom solutions</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">ownership</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">motorsport</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">dealership</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">museum</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">store</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">design</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">innovation & excellence</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">sustainability</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">driving programs</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">lounge</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">club</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">history</a></li>
+						<li className="menu__item"><a className="link-2 link-underline letterspacing-4" href="/">news</a></li>
 					</ul>
-					<div className="menu__divider" aria-hidden={true}><span></span></div>
+					<div className="divider" aria-hidden={true}><div className="divider__line divider__line--overflowing"></div></div>
 					<div className="menu__grid">
 						<div className="menu__item">
 							<h2 className="title-preferences text-neutral-3">languages</h2>
@@ -41,61 +41,61 @@ export default function Menu(props) {
 							<div className="cluster">
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our instagram profile</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-instagram"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our facebook profile</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-facebook"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our youtube channel</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-youtube"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our linkedin profile</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-linkedin"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our wechat profile</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-wechat"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our discord community chat</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-discord"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our slack community chat</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-slack"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our spotify profile</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-spotify"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our reddit</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-reddit"/>
 									</svg>
 								</a>
 								<a href="/" className="link-2">
 									<span className="sr-only">click to open our snapchat profile</span>
-									<svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20">
+									<svg aria-hidden="true" width="24" height="24" viewBox="0 0 20 20">
 										<use href="media/sprites.svg#icon-snapchat"/>
 									</svg>
 								</a>
