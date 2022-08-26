58928e778be7cee49df0d28a5ec2ea433fa8a06c
Martin Kamír <martin.k.kamir@gmail.com>
2022-08-26 17:54:58 +0200

    slider functionality

--- src/react/components/Models.jsx
+++ src/react/components/Models.jsx
@@ -1,63 +1,136 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, {useRef, useState} from "react";
 import Button from "./Button";
 import {typewriterAnimation} from "../Utilities";
 
 export default function Models() {
-	const [items, setItems] = useState([]);
 	const itemsRef = useRef(null);
+	const [count, setCount] = useState(0);
+	const [countA, setCountA] = useState(0);
 
-	function itemHTML(i, isActive) {
-		return (<div className="slider__item" key={i} id={i} data-active={isActive ? true : false}>
-			<div className="slider__skew">
-				<div className="slider__skew-fix">
-					<img src={`./media/home/image-slider-${i}.webp`} alt=""/>
-				</div>
-			</div>
-		</div>)
-	}
-
-	useEffect(() => {
-		// setItems(Array.from({length: 3}, (_, i) => itemHTML(i)));
-		setItems([itemHTML(0, false), itemHTML(1, false), itemHTML(2, true)]);
-	}, []);
-
-	function setActiveItem() {
-		// itemsRef.current.children[0].removeAttribute('data-active');
-		// itemsRef.current.children[1].dataset.active = true;
-		Array.from(itemsRef.current.children).forEach((curItem, i) => {
-			curItem.dataset.active = i === 1 ? true : false;
-		});
-	}
 
 	function handleSlideLeft() {
-		setItems([itemHTML(0, false), itemHTML(1, false), itemHTML(2, true)]);
-		console.log(items);
-		// setItems(items.concat(items.shift()));
+		const slide0 = document.getElementById("slide-0");
+		const slide1 = document.getElementById("slide-1");
+		const slide2 = document.getElementById("slide-2");
+
+		if (countA === 0) {
+			itemsRef.current.insertAdjacentHTML("beforeend", slide0.outerHTML);
+
+			setTimeout(() => {
+				slide2.classList.remove("active");
+				itemsRef.current.children[3].classList.add("active");
+			}, 100);
+
+			setTimeout(() => {
+				itemsRef.current.children[0].remove()
+			}, 500);
+
+			setCount(2)
+			setCountA(1)
+		}
+
+
+
+		if (countA === 1) {
+			itemsRef.current.insertAdjacentHTML("beforeend", slide1.outerHTML);
+
+			setTimeout(() => {
+				slide0.classList.remove("active");
+				itemsRef.current.children[3].classList.add("active");
+			}, 100);
+
+			setTimeout(() => {
+				itemsRef.current.children[0].remove()
+			}, 500);
+
+			setCount(1)
+			setCountA(2)
+		}
+
+		if (countA === 2) {
+			itemsRef.current.insertAdjacentHTML("beforeend", slide2.outerHTML);
+
+			setTimeout(() => {
+				slide1.classList.remove("active");
+				itemsRef.current.children[3].classList.add("active");
+			}, 100);
+
+			setTimeout(() => {
+				itemsRef.current.children[0].remove()
+			}, 500);
+
+			setCount(0)
+			setCountA(0)
+		}
 	}
 
 	function handleSlideRight() {
-		setItems([
-			itemHTML(2, false),
-			itemHTML(0, false),
-			itemHTML(1, true),
-			itemHTML(2, false)
-		]);
+		const slide0 = document.getElementById("slide-0");
+		const slide1 = document.getElementById("slide-1");
+		const slide2 = document.getElementById("slide-2");
+
+		if (count === 0) {
+			slide1.classList.add("active");
+			slide2.classList.remove("active");
+			itemsRef.current.insertAdjacentHTML("afterbegin", slide2.outerHTML);
+
+
+			setCountA(2)
+			setCount(1);
+		}
+
+		if (count === 1) {
+			slide0.classList.add("active");
+			slide1.classList.remove("active");
+			itemsRef.current.insertAdjacentHTML("afterbegin", slide1.outerHTML);
+
+			setCountA(1);
+			setCount(2);
+		}
+
+		if (count === 2) {
+			slide2.classList.add("active");
+			slide0.classList.remove("active");
+			itemsRef.current.insertAdjacentHTML("afterbegin", slide0.outerHTML);
+
+			setCountA(0);
+			setCount(0);
+		}
 
 		setTimeout(() => {
-			setItems([
-			itemHTML(2, false),
-			itemHTML(0, false),
-			itemHTML(1, true),
-		]);
-		}, 500)
-		// setItems([items.pop()].concat(items));
+			itemsRef.current.children[3].remove()
+		}, 500);
 	}
 
 	return (<section className="slider surface-neutral-6 text-neutral-1">
-		<div className="slider__items" ref={itemsRef}>
+		<div className="slider__items">
 			<div className="slider__slider">
-				<div className="slider__slides">
-					{items}
+				<div className="slider__slides" ref={itemsRef}>
+
+					<div className="slider__item" id="slide-0" data-active={false}>
+						<div className="slider__skew">
+							<div className="slider__skew-fix">
+								<img src={`./media/home/image-slider-0.webp`} alt=""/>
+							</div>
+						</div>
+					</div>
+
+					<div className="slider__item" id="slide-1" data-active={false}>
+						<div className="slider__skew">
+							<div className="slider__skew-fix">
+								<img src={`./media/home/image-slider-1.webp`} alt=""/>
+							</div>
+						</div>
+					</div>
+
+					<div className="slider__item active" id="slide-2" data-active={true}>
+						<div className="slider__skew">
+							<div className="slider__skew-fix">
+								<img src={`./media/home/image-slider-2.webp`} alt=""/>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
